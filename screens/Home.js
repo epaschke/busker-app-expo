542dc57fbfaed1067b38c7a6dec77bd0edddeb5e
Bianca-Morris <bmorris@wellesley.edu>
2017-10-20 21:11:33 -0700

    fix scrolling on home screen

--- screens/Home.js
+++ screens/Home.js
@@ -26,25 +26,75 @@ export default class Home extends React.Component {
                 <Text style={styles.headingText}>Followed</Text>
               </View>
               {/* <View style={{flex: 1, flexDirection: 'column', alignItems: 'flex-start'}}> */}
-                <ScrollView horizontal={true}
-                  decelerationRate={0}
-                  snapToInterval={200} //your element width
-                  snapToAlignment={"center"}>
-                  <View style={{alignItems: 'flex-start'}}>
-                    <View style={{justifyContent: 'center'}}>
-                      <Image style={styles.image}
-                        source={require('../img/Kamikaze-Fireflies-3.jpg')} />
-                        <Text style={{paddingTop: 7, paddingLeft: 34, fontSize: 12}}>@StreetPerformer1</Text>
-                        <Text style={{paddingLeft: 15, fontSize: 12}}>Circus Art, Flame Thrower</Text>
-                    </View>
-                    <View style={{justifyContent: 'center'}}>
-                      <Image style={styles.image}
-                        source={require('../img/breakfree.jpg')} />
-                        <Text style={{paddingTop: 7, paddingLeft: 34, fontSize: 12}}>@BreakFreeHipHop</Text>
-                        <Text style={{marginLeft: 30, fontSize: 12}}>Dance, Hip Hop</Text>
-                      </View>
-                    </View>
-                </ScrollView>
+              <Text style={styles.headingText}>Followed</Text>
+            {/* <View style={{flex: 1, flexDirection: 'column', alignItems: 'flex-start'}}> */}
+            <ScrollView>
+              <ScrollView horizontal={true}
+                decelerationRate={0}
+                snapToInterval={200} //your element width
+                snapToAlignment={"center"}>
+                <View style={{justifyContent: 'center'}}>
+                  <Image style={styles.image}
+                  source={require('./img/Kamikaze-Fireflies-3.jpg')} />
+                  <Text style={{paddingTop: 7, paddingLeft: 34, fontSize: 12}}>@StreetPerformer1</Text>
+                  <Text style={{paddingLeft: 15, fontSize: 12}}>Circus Art, Flame Thrower</Text>
+                </View>
+                <View style={{justifyContent: 'center'}}>
+                  <Image style={styles.image}
+                  source={require('./img/breakfree.jpg')} />
+                  <Text style={{paddingTop: 7, paddingLeft: 34, fontSize: 12}}>@BreakFreeHipHop</Text>
+                  <Text style={{marginLeft: 30, fontSize: 12}}>Dance, Hip Hop</Text>
+                </View>
+                <View style={{justifyContent: 'center'}}>
+                  <Image style={styles.image}
+                  source={require('./img/breakfree.jpg')} />
+                  <Text style={{paddingTop: 7, paddingLeft: 34, fontSize: 12}}>@BreakFreeHipHop</Text>
+                  <Text style={{marginLeft: 30, fontSize: 12}}>Dance, Hip Hop</Text>
+                </View>
+                <View style={{justifyContent: 'center'}}>
+                  <Image style={styles.image}
+                  source={require('./img/breakfree.jpg')} />
+                  <Text style={{paddingTop: 7, paddingLeft: 34, fontSize: 12}}>@BreakFreeHipHop</Text>
+                  <Text style={{marginLeft: 30, fontSize: 12}}>Dance, Hip Hop</Text>
+                </View>
+            </ScrollView>
+            <Text style={styles.headingText}>Nearby</Text>
+            <ScrollView horizontal={true}
+                decelerationRate={0}
+                snapToInterval={200} //your element width
+                snapToAlignment={"center"}>
+                <View style={{justifyContent: 'center'}}>
+                  <Image style={styles.image}
+                  source={require('./img/Kamikaze-Fireflies-3.jpg')} />
+                  <Text style={{paddingTop: 7, paddingLeft: 34, fontSize: 12}}>@StreetPerformer1</Text>
+                  <Text style={{paddingLeft: 15, fontSize: 12}}>Circus Art, Flame Thrower</Text>
+                </View>
+                <View style={{justifyContent: 'center'}}>
+                  <Image style={styles.image}
+                  source={require('./img/breakfree.jpg')} />
+                  <Text style={{paddingTop: 7, paddingLeft: 34, fontSize: 12}}>@BreakFreeHipHop</Text>
+                  <Text style={{marginLeft: 30, fontSize: 12}}>Dance, Hip Hop</Text>
+                </View>
+            </ScrollView>
+            <Text style={styles.headingText}>Featured</Text>
+            <ScrollView horizontal={true}
+                decelerationRate={0}
+                snapToInterval={200} //your element width
+                snapToAlignment={"center"}>
+                <View style={{justifyContent: 'center'}}>
+                  <Image style={styles.image}
+                  source={require('./img/Kamikaze-Fireflies-3.jpg')} />
+                  <Text style={{paddingTop: 7, paddingLeft: 34, fontSize: 12}}>@StreetPerformer1</Text>
+                  <Text style={{paddingLeft: 15, fontSize: 12}}>Circus Art, Flame Thrower</Text>
+                </View>
+                <View style={{justifyContent: 'center'}}>
+                  <Image style={styles.image}
+                  source={require('./img/breakfree.jpg')} />
+                  <Text style={{paddingTop: 7, paddingLeft: 34, fontSize: 12}}>@BreakFreeHipHop</Text>
+                  <Text style={{marginLeft: 30, fontSize: 12}}>Dance, Hip Hop</Text>
+                </View>
+            </ScrollView>
+        </ScrollView>
                 {/* </View> */}
             </View>
         )
@@ -53,21 +103,25 @@ export default class Home extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
-  headingText: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 40,
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-  image: {
-    width: 150,
-    height: 150,
-    marginLeft: 15,
-    marginTop: 20,
-    borderRadius: 5
-  }
+  flex: 1,
+  backgroundColor: '#fff',
+  paddingTop: Constants.statusBarHeight
+},
+headingText: {
+  textAlign: 'center',
+  alignItems: 'center',
+  justifyContent: 'center',
+  marginTop: 10,
+  marginBottom: 0,
+  fontWeight: 'bold',
+  fontSize: 20,
+  fontFamily: 'Helvetica Neue',
+},
+image: {
+  width: 150,
+  height: 150,
+  marginLeft: 15,
+  marginTop: 20,
+  borderRadius: 5
+}
 });
