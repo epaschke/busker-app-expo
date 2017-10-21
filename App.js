46898cdb92cb3a9cd59933736e088579d9cacf81
Emma Paschke <epaschke>
2017-10-20 20:34:22 -0700

    adds navigator routes

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './screens/LoginScreen';
+import MapScreen from './screens/MapScreen';
+
 import { StackNavigator } from 'react-navigation';
 
 const Navigator = StackNavigator({
-    Login: {screen: LoginScreen}
+    Login: {screen: LoginScreen},
+    Map: {screen: MapScreen}
+}, {
+    initialRouteName: 'Login'
 })
 
 export default Navigator;
