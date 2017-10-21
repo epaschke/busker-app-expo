56781e523464ef43a6331b8e88489398b7978f82
Symonne Singleton <symonne.singleton@gmail.com>
2017-10-20 23:02:56 -0700

    hello

--- App.js
+++ App.js
@@ -1,10 +1,99 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './screens/LoginScreen';
+import { StyleSheet, Text, View, AppRegistry, Image, Button } from 'react-native';
+// import LoginScreen from './screens/LoginScreen';
 import { StackNavigator } from 'react-navigation';
+// import ProfileHeader from './screens/ProfileHeader';
 
-const Navigator = StackNavigator({
-    Login: {screen: LoginScreen}
-})
 
-export default Navigator;
+class PerformerProfile extends React.Component {
+    static navigationOptions = {
+    title: 'PerformerProfile'
+  };
+
+      render(){
+          return (
+            <View style={styles.container}>
+
+              <ProfileHeader />
+            </View>
+          );
+      }
+  }
+
+class ProfileHeader extends React.Component {
+    static navigationOptions = {
+    title: ProfileHeader
+  };
+      render(){
+          return (
+
+  <Image style={styles.headerBackground} source={require('./img/moon.jpg')} >
+    <View style={styles.header}>
+      <View style={styles.profilepicWrap}>
+        <Image style={styles.profilepic} source={require('./img/moon.jpg')}> </Image>
+      </View>
+      <Text style={styles.name}>Beyoncè</Text>
+      <Text style={style.pos}> Singer / Entertainer </Text>
+    </View>
+  </Image>
+
+          );
+      }
+  }
+
+  let styles = StyleSheet.create({
+    headerBackground:{
+      flex: 1,
+      width: null,
+      alignSelf: 'stretch'
+    },
+    header: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 20,
+      backgroundColor: '#A9AB94',
+    },
+    container: {
+      flex: 1,
+      backgroundColor: '#000',
+    },
+    profilepicWrap: {
+      width: 180,
+      height: 180,
+      borderRadius: 100,
+      borderColor: '#313C41',
+      borderWidth: 16,
+    },
+    profilepic: {
+      flex: 1,
+      width: null,
+      alignSelf: 'stretch',
+      borderRadius: 100,
+      borderColor: '#FED9A7',
+      borderWidth: 4
+    },
+    name: {
+      marginTop: 20,
+      fontSize: 16,
+      color: '#F47056',
+      fontWeight: 'bold'
+    },
+    pos: {
+      fontSize: 14,
+      color: '#A9AB94',
+      fontWeight: '300',
+      fontStyle: 'italic'
+    },
+
+  });
+
+
+  export default StackNavigator({
+   PerformerProfile: {
+     screen: PerformerProfile,
+   },
+    ProfileHeader: {
+      screen: ProfileHeader,
+    },
+}, {initialRouteName: 'PerformerProfile'});
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -17,9 +17,16 @@ export default class LoginScreen extends React.Component {
 
     render(){
         return (
-            <View style={styles.container}>
-                <Text>Login Screen</Text>
-            </View>
+          <View style={styles.container}>
+            <Text style={styles.textBig}>BEAT</Text>
+            {/* make this an icon */}
+            <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
+              <Text style={styles.buttonLabel}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.register()} }>
+              <Text style={styles.buttonLabel}>Register</Text>
+            </TouchableOpacity>
+          </View>
         )
     }
 }
@@ -31,4 +38,49 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  input: {
+  margin: 15,
+  height: 40,
+  borderColor: '#7a42f4',
+  borderWidth: 1
+ },
+
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  textBig: {
+    fontSize: 36,
+    textAlign: 'center',
+    margin: 10,
+  },
+  button: {
+    alignSelf: 'stretch',
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 10,
+    marginLeft: 5,
+    marginRight: 5,
+    borderRadius: 5
+  },
+  buttonRed: {
+    backgroundColor: '#FF585B',
+  },
+  buttonBlue: {
+    backgroundColor: '#0074D9',
+  },
+  buttonGreen: {
+    backgroundColor: '#2ECC40'
+  },
+  buttonLabel: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: 'white'
+  }
+ });
